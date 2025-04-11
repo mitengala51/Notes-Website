72ef18a0fe67f7c494b929ddcb164061576b042f
mitengala51 <mitengala51@gmail.com>
2025-04-11 17:10:30 +0530

    Edit ,Delete and Media Queries Added

--- frontend/src/Components/Note.jsx
+++ frontend/src/Components/Note.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import './Note.css'
+import React from "react";
+import "./Note.css";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function Note() {
   return (
-    <div className='note'>
-      <h3 className='notes'>first</h3>
-      <p className='notes'>The patient has been diagnosed with pneumonoultramicroscopicsilicovolcanoconiosis and 
-        will be taken into intensive care.</p>
+    <div className="note">
+      <h3 className="notes">first</h3>
+      <p className="notes">
+        The patient has been diagnosed with
+        pneumonoultramicroscopicsilicovolcanoconiosis and will be taken into
+        intensive care.
+      </p>
+      <div className="icons">
+        <EditIcon style={{marginLeft: "2px", marginRight: "2px"}}/>
+        <DeleteIcon />
+      </div>
     </div>
-  )
+  );
 }
